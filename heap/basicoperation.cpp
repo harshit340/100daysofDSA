4fe141a01a90d87b6bc425a25afdf76fafd75efa
harshit <harshitshrivastava340@gmail.com>
2024-03-04 01:53:37 +0530

    heapify

--- heap/basicoperation.cpp
+++ heap/basicoperation.cpp
@@ -68,7 +68,30 @@ class heap{
     }
 
     }
+
+
+
+
 };
+
+void heapify(int arr[],int n,int i){
+    int largest = i;
+    int left = 2*i;
+    int right = 2*i + 1;
+
+    if(left<n && arr[largest]<arr[left]){
+        largest = left;
+    }
+    if(right<n && arr[largest]<arr[right]){
+        largest = right;
+    }
+
+    if(largest !=i){
+        swap(arr[largest],arr[i]);
+        heapify(arr ,n , largest);
+    }
+
+}
 
 int main(){
     heap h;
@@ -84,5 +107,21 @@ int main(){
     h.deletefromheap();
     h.print();
     
+
+  int arr[6]={-1,45,54,34,23,67};
+  int n=6;
+  for(int i =n/2 ; i>0 ;i--)
+  {
+    heapify(arr,n,i);
+   
+  }
+
+  cout<<"printing your array now"<<endl;
+
+  for(int i=1;i<n;i++){
+    cout<<arr[i]<<" ";
+  }
+
+
     return 0;
 }
