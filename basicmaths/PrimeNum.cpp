f51ea3e20ffa2256854636d2de994574fe2cf89e
harshit <harshitshrivastava340@gmail.com>
2024-04-05 03:48:48 +0530

    sieve

--- basicmaths/PrimeNum.cpp
+++ basicmaths/PrimeNum.cpp
@@ -17,3 +17,28 @@ bool isPrime(int n)
 		return false;
 	}
 }
+
+// sieve of eratosthenes 
+
+
+bool isPrime(int n)
+{
+     bool isitprime[n+1];
+
+	 for(int i=0;i<n;i++)
+	 {
+		 isitprime[i]=true;
+	 }
+
+	 isitprime[0]=false;
+	 isitprime[1]=false;
+     
+	 for(int i=2;i*i<n;i++){
+		 if(isitprime[i]==true){
+			 for(int j=i*i;j<n;j+=i){
+				 isitprime[j]=false;
+			 }
+		 }
+	 }
+
+}
